4db6b97f4f7c332908575194cc824335da4e706f
BRI4MZP <riveirodelrio@gmail.com>
2023-04-19 18:02:39 +0200

    doing

--- spyder2.py
+++ spyder2.py
@@ -24,16 +24,35 @@ def getArgs():
 # 	for page in htmlresponse.findAll('a', href=True):
 # 		urls.add(page['href'])
 # 	# print(urls)
+def maker(actually, next):
+    actually = urlparse(actually)
+    next = urlparse(next)
+    protocolo, dominio, path, params = next.scheme, next.netloc, next.path, next.params
+    if protocolo == '':
+        protocolo = actually.scheme
+    if dominio == '':
+        dominio = actually.netloc
+    if path == '':
+        path = actually.path
+    if params == '':
+        params = actually.params
+    adv = protocolo + '://' + dominio + path + params
+    return str(adv)
+
 def getUrls(url, level):
 	try:
 		response = requests.get(url, timeout=5)
 		print('hols')
 		if response.status_code == 200:
 			htmlresponse = paco(response.text, 'html.parser')
-			print('eh')
 			for x in htmlresponse.findAll('a', href=True):
-				if "https" in str(x):
-					print(str(x.get("href")) + '\n')
+				url = maker(url, x['href'])
+				if url not in urls:
+					urls.add(url)
+					print(url)
+					print(level)
+				if level < args.l:
+					getUrls(url, level+1)
 	except Exception:
 		print(level, Exception.args)
 
